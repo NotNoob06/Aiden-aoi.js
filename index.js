6eae08b67a0fd95684b44f4a17b0b25d5b2acdd7
Neo <71941576+NeoAIChan@users.noreply.github.com>
2021-10-20 00:33:47 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const keepAlive = require('./server.js') //This is for making the bot alive 7/24 on repl.it
+const Aoijs = require('aoi.js')
+const bot = new Aoijs.Bot({
+  token: process.env.TOKEN,
+  prefix: "/", 
+  fetchInvites: true
+})
+
+require('./callbacks')(bot)
+
+bot.variables({
+	safe: "B2FFB2", 
+	idle: "FFE2CB", 
+	dang: "FEB2B2",
+        invite: "0", 
+	streak: "0", 
+        point: "0", 
+	cd: "0"
+})
+
+bot.status(require('./status.js'))
+const status = require("./status.js")
+
+keepAlive() 
