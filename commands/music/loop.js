b3dd7156566e4de927f292031581bc4f907e4d94
Neo <71941576+NeoAIChan@users.noreply.github.com>
2021-10-20 01:49:16 +0300

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -7,11 +7,12 @@ $interactionReply[You must join voice chat.;;;64]
 
 $else
 $buttonCollector[$get[id];$authorID;1m;1,2,3,;none,loop,cancel;Only $username can use this interaction,,64]
-
 $interactionReply[You are the only one who can use it;;;64]
 $let[id;$apiMessage[$channelId;;{title:𓂃loop}{description:please pick one button for loop, it is **__$loopStatus__** now. }
 {color:$getServerVar[idle]};{actionRow:loop none,2,2,1,,false:loop queue, 2,2,2,,false:cancel,2,4,3,,false};;yes]]
+
 $endif
+
 $suppressErrors[Nothing is playing right now.]
 `
 }, {
@@ -22,10 +23,12 @@ $if[$checkCondition[$loopQueue==false]]
 
 $interactionReply[;{title:𓂃loop}{description:loop picked!}
 {field:𓂃current loop status: $loopStatus}{color:$getServerVar[safe]};{actionRow:loop none,2,3,1,,true:loop queue,2,2,2,,true:cancel,2,4,3,,true};;7]
+
 $else
 $loopQueue
 $interactionReply[;{title:𓂃loop}{description:loop picked!}
 {field:𓂃current loop status: $loopStatus}{color:$getServerVar[safe]};{actionRow:loop none,2,3,1,,true:loop queue,2,2,2,,true:cancel,2,4,3,,true};;7]
+
 $endif
 `
 }, {
@@ -34,9 +37,11 @@ $endif
   code: `
 $if[$checkCondition[$loopQueue==true]]
 $interactionReply[;{title:𓂃loop}{description:looping picked!}{field:𓂃current loop status:$loopStatus}{color:$getServerVar[safe]};{actionRow:loop none,2,2,1,,true:loop queue,2,3,2,,true:cancel,2,4,3,,true};;7]
+
 $else
 $loopQueue
 $interactionReply[;{title:𓂃loop}{description:looping picked!}{field:𓂃current loop status:$loopStatus}{color:$getServerVar[safe]};{actionRow:loop none,2,2,1,,true:loop queue,2,3,2,,true:cancel,2,4,3,,true};;7]
+
 $endif
 `
 }, {
