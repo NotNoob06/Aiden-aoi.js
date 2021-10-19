ebb8d13b2bf8dc0c5cf2ccf5c852dce02276b42b
Neo <71941576+NeoAIChan@users.noreply.github.com>
2021-10-20 00:50:50 +0300

    Update avatar.js

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "avatar",
   type: 'interactionCommand', 
   code:`
-$interactionReply[;{description:**𓂃nickname:** __$nickname[$findUser[$message]]__
-**𓂃user tag:** __$userTag[$findUser[$message]]__}{image:$userAvatar[$findUser[$message[1]]]}{color:$getServerVar[dang]};;;]
+$interactionReply[{description:**𓂃nickname:** __$nickname[$findUser[$message]]__
+**𓂃user tag:** __$userTag[$findUser[$message]]__}{image:$userAvatar[$findUser[$message[1]]]}{color:$getServerVar[dang]}]
 `
  }
