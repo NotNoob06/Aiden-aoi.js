fa57adc4717101232f90aca2bf1db4daebc0dca3
Neo <71941576+NeoAIChan@users.noreply.github.com>
2021-10-20 00:57:41 +0300

    Create daily.js

---
+++ commands/general/daily.js
@@ -0,0 +1,27 @@
+module.exports = [{
+name: "daily",
+type: 'interactionCommand', 
+code: `
+$if[$sub[$dateStamp;$getUserVar[cd]]>0]
+$setTimeOut[48h;
+user: $authorID
+streak: $getUserVar[streak]]
+
+$interactionReply[You collected your daily balance and now you have **$sum[$getUserVar[point];100] (+$getUserVar[streak] streaks) MNS.**]
+
+$setUserVar[streak;$sum[$getUserVar[streak];1]]
+
+$setUserVar[cd;$sum[86400000;$dateStamp]]
+
+$setUserVar[point;$sum[$getUserVar[point];100;$multi[2;$getUserVar[streak]]]]
+$else
+$interactionReply[You already claimed your chest, wait **$humanizeMs[$sub[$getUserVar[cd];$dateStamp]]** time to get your next chest.;;;64]
+$endif
+`
+}, {
+type: 'timeoutCommand', 
+code: `
+$setUserVar[streak;$sum[$getUserVar[streak];1]]
+
+$onlyIf[$timeoutdata[streak]==$getUserVar[streak;$timeoutdata[user]];]`
+}]
