be74d6a2daa4cc56d0aac12aaedf3f7a043b6fdb
Neo <71941576+NeoAIChan@users.noreply.github.com>
2021-10-20 01:59:02 +0300

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -5,10 +5,6 @@ module.exports = {
 $sendDM[$findUser[$message[1]];{author:First, Hello...:$serverIcon}{description:*You got ban from __$serverName__.*}{field:reason: ||$replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;No Reason Given];false;$messageSlice[1]]||}{color:$getServerVar[dang]}{thumbnail:$userAvatar[$findUser[$message[1]]]}{timestamp}]
 $onlyIf[$isUserDMEnabled[$findUser[$message[1]]]!=false;Couldn't send the reason via DM, their DM closed.]
 
-$channelSendMessage[879963537345224715;{author:Ban:$userAvatar[$clientID]}{field:𓂃banned id:$findUser[$message[1]]}
-{field:𓂃banned user:$userTag[$findUser[$message[1]]]}
-{field:𓂃banned by:$userTag[$authorID]}{field:𓂃reason: $replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;no reason given];false;$messageSlice[1]]}{thumbnail:$userAvatar[$findUser[$message[1]]]}{color:$getServerVar[dang]}{timestamp}]
-
 $ban[$findUser[$message[1]];$messageSlice[1]]
 
 $interactionReply[{author:banned successfully:$authorAvatar}{field:𓂃banned user: $userTag[$findUser[$message[1]]]}{field:𓂃banned by: $userTag[$authorID]}{color:$getServerVar[safe]}{thumbnail:$userAvatar[$findUser[$message[1]]]}{footer:Reason, $replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;No Reason Given];false;$messageSlice[1]]}{timestamp}]
